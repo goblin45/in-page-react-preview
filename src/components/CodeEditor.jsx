65e2b2fa1212ffa523681f84fdd3ce7d522e0e6d
Rajarshi Dutta <gogolmf74@gmail.com>
2024-06-28 20:02:33 +0530

    fix css

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -4,10 +4,12 @@ import { Editor } from '@monaco-editor/react';
 const CodeEditor = ({ code, setCode }) => {
   return (
     <Editor
-      height="90vh"
+      height="100%"
+      width={'100%'}
       defaultLanguage="typescript"
       value={code}
       onChange={(value) => setCode(value || '')}
+      theme="vs-dark"
     />
   );
 };
